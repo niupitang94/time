4122af13a39377d7e84195fef3d066870e7ec65d
niupitang94 <50044433+niupitang94@users.noreply.github.com>
2019-06-30 23:18:52 +0800

    还是一个空白的登录页

---
+++ Signin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="regin_out">
+    
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {}
+  }
+}
+</script>
+<style>
+.regin_out {
+  width:1000px;
+  height:575px;
+  background-color: #fff;
+  margin:70px auto;
+  border-radius:8px;
+}  
+</style>
